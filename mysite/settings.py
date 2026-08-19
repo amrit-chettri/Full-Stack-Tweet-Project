d7ad5df76ad7bf405f06c3d70d4d6a287128e36e
Amrit Chettri <amrit.git.np@gmail.com>
2026-08-19 20:26:06 +0545

    Change ALLOWED_HOSTS to allow all
    Updated ALLOWED_HOSTS to allow all hosts.

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^=6-_k)oh!n9-fpcd1qd0rf(!8y2!!8cc*so1if(!*ydv@*_dc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://8000-idx-tweet-1732437108058.cluster-7ubberrabzh4qqy2g4z7wgxuw2.cloudworkstations.dev']
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-idx-tweet-1732437108058.cluster-7ubberrabzh4qqy2g4z7wgxuw2.cloudworkstations.dev']
 
